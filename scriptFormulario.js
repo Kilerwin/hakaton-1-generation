5142d2e0ee279cda5fd117c0e33d1a2e0d47a87f
Kilerwin <miguel642013@gmail.com>
2025-04-22 17:08:34 -0500

    correccion errores y arreglo de funcionalidad de agregar el carrito

--- scriptFormulario.js
+++ scriptFormulario.js
@@ -1,14 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("suscribirse").addEventListener('click', function(e){
-        e.preventDefault()
-        const input = document.getElementById("inputemail");
-        const texto = input.value.trim();
-        const mensaje = document.getElementById("printmensaje");
-  
-      if(texto){
-        mensaje.textContent = `Hemos enviado el cupón al correo ${texto}`;
-        input.value = '';
-      } 
-    });
-  });
-  
+document.getElementById("contactform").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  const input = document.getElementById("inputemail");
+  const texto = input.value.trim();
+  const mensaje = document.getElementById("printmensaje");
+
+  if (texto) {
+    mensaje.textContent = `Hemos enviado el cupón al correo ${texto}`;
+    input.value = '';
+  }
+});
